3fc5ee7e6819df1821c3c2ea3315ed1d95a67d43
hoangnhii211203 <hoangnhii211203@gmail.com>
2024-01-31 00:05:52 +0700

    Done Exercise 34 and 35

---
+++ src/Java34/Test.java
@@ -0,0 +1,24 @@
+package Java34;
+
+public class Test {
+    public static void main(String[] args) {
+        MyDate md = new MyDate(15, 5, 2025);
+        MyDate md2 = new MyDate(11, 1, 2021);
+        MyDate md3 = new MyDate(15, 5, 2025);
+
+        System.out.println(md);
+        System.out.println(md2);
+        System.out.println(md3);
+
+        System.out.println("\n-----\n");
+
+        System.out.println("md so sanh bang md2: " + md.equals(md2));
+        System.out.println("md so sanh bang md3: " + md.equals(md3));
+
+        System.out.println("\n-----\n");
+
+        System.out.println("Hashcode md: " + md.hashCode());
+        System.out.println("Hashcode md2: " + md2.hashCode());
+        System.out.println("Hashcode md3: " + md3.hashCode());
+    }
+}
---
+++ src/Java35/Book.java
@@ -0,0 +1,61 @@
+package Java35;
+
+public class Book {
+    private String NameBook;
+    private double Price;
+    private int ReleaseYear;
+    private Author Author;
+
+    public Book(){}
+
+    public Book(String nameBook, double price, int releaseYear, Java35.Author author) {
+        NameBook = nameBook;
+        Price = price;
+        ReleaseYear = releaseYear;
+        Author = author;
+    }
+
+    public String getNameBook() {
+        return NameBook;
+    }
+
+    public void setNameBook(String nameBook) {
+        NameBook = nameBook;
+    }
+
+    public double getPrice() {
+        return Price;
+    }
+
+    public void setPrice(double price) {
+        Price = price;
+    }
+
+    public int getReleaseYear() {
+        return ReleaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        ReleaseYear = releaseYear;
+    }
+
+    public Java35.Author getAuthor() {
+        return Author;
+    }
+
+    public void setAuthor(Java35.Author author) {
+        Author = author;
+    }
+
+    public void inTenSach(){
+        System.out.println("Ten sach: " + getNameBook());
+    }
+
+    public boolean kiemTraCungNam(Book OtherBook){
+        return this.getReleaseYear() == OtherBook.getReleaseYear();
+    }
+
+    public double giaSauKhiGiam(double x){
+        return getPrice() - (getPrice()*x/100);
+    }
+}
